5186a517c40d6a9c523101c8dda6b11bd6b3ada2
jshyam9384-commits <jshyam9384@gmail.com>
2026-09-14 21:31:28 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,36 @@
+#1.arithematic operator
+from operator import truediv
+
+# a=10
+# b=3
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)      # reminder
+# print(a**b)     # a^3
+#print(a//b)     # removing the decimal after the division
+
+#2.comprasion operator
+   # it shows  the comparison btw to function using true or false
+
+# x=5
+# y=10
+#
+# print(x==y)
+# print(x!=y)    #not equal to
+# print(x>y)
+# print(x<y)
+
+#3.logical operator
+#
+# g=True
+# v=False
+#
+# print(g and v)  #(and== true if both are true )
+# print(g or v)   #(or==  true when atleast onside is true)
+# print(not g)      #(not== reverse the value)
+# print(not v)
+# print(not not g)
+# print(not g and g)
+# print(not v and g)
